0df79b8d374e186378b70358c2dfcf7cff4f4b1d
Kelindil <76110023+Kelindil@users.noreply.github.com>
2020-12-19 22:14:35 +0100

    Summative version, no error

--- OurGame/Assets/Ksenia/PlayerController.cs
+++ OurGame/Assets/Ksenia/PlayerController.cs
@@ -5,123 +5,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
-    public float jumpForce;
-    Rigidbody2D rb;
-    float xInput;
-    bool isGrounded;
+    public float moveSpeed;
+    public float jumpPower;
 
-    bool isJumping = false;
-    bool isRunning = false;
-    bool isIdle = false;
+    private Rigidbody2D rb;
 
-
-    private Animator animator;
-    // Start is called before the first frame update
-
-    private SpriteRenderer spriteRenderer;
+    bool isGrounded = false;
 
     void Start()
     {
-        xInput = 0.0f;
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        isGrounded = false;
-
-        animator = gameObject.GetComponent<Animator>();
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxis("Horizontal");
 
-        if (xInput != 0)
-        {
-            isIdle = false;
-
-            //if the player is on the ground
-            if (isGrounded == true)
-            {
-                isRunning = true;
-                isJumping = false;
-            }
-
-            //if the player is not on the ground
-            else if (isGrounded == false)
-            {
-                isRunning = false;
-                isJumping = true;
-            }
-
-            //if xInput is positive, the player moving right
-            if (xInput > 0f)
-            {
-                spriteRenderer.flipX = false;
-            }
-
-            //if xInput is negative, the player moving left
-            else if (xInput < 0f)
-            {
-                spriteRenderer.flipX = true;
-            }
-        }
-
-        //if the player is not giving x input
-        else
-        {
-
-            if (isGrounded == true)
-            {
-                isIdle = true;
-                isRunning = false;
-                isJumping = false;
-            }
-        }
-
-        //if xInput, then move the charecter = xInput * speed; 
-        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
-
-        if (Input.GetAxis("Jump") > 0f && isGrounded == true)
-        {
-            //player is jumping
-            isJumping = true;
-            isRunning = false;
-            isIdle = false;
-
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-
-        }
-
-        animator.SetBool("isRunning", isRunning); // connect var from Unity and Visual studio
-        animator.SetBool("isJumping", isJumping);
-        animator.SetBool("isIdle", isIdle);
+        Vector3 Move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += Move * Time.deltaTime * moveSpeed;
+        Jump();
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    void Jump()
     {
-        if (collision.gameObject.tag == "Ground")
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
-            isGrounded = true;
-            isJumping = false;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
+            rb.AddForce(new Vector2(0f, jumpPower), ForceMode2D.Impulse);
             isGrounded = false;
-            isJumping = true;
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D other)
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            //player takes 1 point of fall damage
-            gameObject.GetComponent<PlayerHealth>().ModifyHealth(-1);
-        }
+        isGrounded = true;
     }
 }
